2d0093edaf064900f7a06ff21bbd1d170b8c84ed
Alexander Novikov <aliaksandr_novikau1@epam.com>
2021-01-11 11:25:51 +0300

    version 1.1

--- src/main/java/com/epam/esm/configuration/GiftCertificateMapper.java
+++ src/main/java/com/epam/esm/configuration/GiftCertificateMapper.java
@@ -12,22 +12,35 @@ import java.util.TimeZone;
 
 @Component
 public class GiftCertificateMapper implements RowMapper<GiftCertificate> {
+
+    private static final String ID = "id";
+    private static final String NAME = "name";
+    private static final String DESCRIPTION = "description";
+    private static final String PRICE = "price";
+    private static final String DURATION = "duration";
+    private static final String CREATE_DATE = "create_date";
+    private static final String LAST_UPDATE_DATE = "last_update_date";
+
+    private static final String TIMEZONE_EAT = "EAT";
+    private static final String PATTERN_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+
+
     @Override
     public GiftCertificate mapRow(ResultSet rs, int rowNum) throws SQLException {
         GiftCertificate giftCertificate = new GiftCertificate();
-        giftCertificate.setId(rs.getInt("id"));
-        giftCertificate.setName(rs.getString("name"));
-        giftCertificate.setDescription(rs.getString("description"));
-        giftCertificate.setPrice(rs.getInt("price"));
-        giftCertificate.setDuration(rs.getInt("duration"));
+        giftCertificate.setId(rs.getInt(ID));
+        giftCertificate.setName(rs.getString(NAME));
+        giftCertificate.setDescription(rs.getString(DESCRIPTION));
+        giftCertificate.setPrice(rs.getInt(PRICE));
+        giftCertificate.setDuration(rs.getInt(DURATION));
 
 
-        TimeZone tz = TimeZone.getTimeZone("EAT");
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss");
+        TimeZone tz = TimeZone.getTimeZone(TIMEZONE_EAT);
+        DateFormat df = new SimpleDateFormat(PATTERN_DATE_FORMAT);
         df.setTimeZone(tz);
 
-        String createDate = df.format(rs.getTimestamp("create_date"));
-        String lastUpdateDate = df.format(rs.getTimestamp("last_update_date"));
+        String createDate = df.format(rs.getTimestamp(CREATE_DATE));
+        String lastUpdateDate = df.format(rs.getTimestamp(LAST_UPDATE_DATE));
 
         giftCertificate.setCreateDate(createDate);
         giftCertificate.setLastUpdateDate(lastUpdateDate);
--- src/main/java/com/epam/esm/configuration/MyConfig.java
+++ src/main/java/com/epam/esm/configuration/MyConfig.java
@@ -14,14 +14,19 @@ import javax.sql.DataSource;
 @EnableWebMvc
 public class MyConfig {
 
+    private static final String DRIVER_CLASS_NAME = "com.mysql.jdbc.Driver";
+    private static final String URL = "jdbc:mysql://localhost/mjc_module_2?useSSl=false&serverTimezone=Europe/Moscow";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "12345";
+
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
 
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        dataSource.setUrl("jdbc:mysql://localhost/mjc_module_2?useSSl=false&serverTimezone=Europe/Moscow");
-        dataSource.setUsername("root");
-        dataSource.setPassword("12345");
+        dataSource.setDriverClassName(DRIVER_CLASS_NAME);
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
 
         return dataSource;
     }
