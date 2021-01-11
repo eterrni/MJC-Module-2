9dfac0b87b26dd3603ba0b9ffdb1eb6bbc98918d
Alexander Novikov <aliaksandr_novikau1@epam.com>
2021-01-11 12:41:49 +0300

    version 1.1.1

--- src/main/java/com/epam/esm/configuration/MyConfig.java
+++ src/main/java/com/epam/esm/configuration/MyConfig.java
@@ -1,13 +1,14 @@
 package com.epam.esm.configuration;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
 
 @Configuration
 @ComponentScan(basePackages = "com.epam.esm")
@@ -21,13 +22,15 @@ public class MyConfig {
 
     @Bean
     public DataSource dataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-
-        dataSource.setDriverClassName(DRIVER_CLASS_NAME);
-        dataSource.setUrl(URL);
-        dataSource.setUsername(USERNAME);
-        dataSource.setPassword(PASSWORD);
-
+        ComboPooledDataSource dataSource = new ComboPooledDataSource();
+        try {
+            dataSource.setDriverClass(DRIVER_CLASS_NAME);
+            dataSource.setJdbcUrl(URL);
+            dataSource.setUser(USERNAME);
+            dataSource.setPassword(PASSWORD);
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
         return dataSource;
     }
 
